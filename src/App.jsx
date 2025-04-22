bf7cbb408b25998b2662544ad6bff761750f5f88
boody <bodymagdy97@gmail.com>
2025-04-23 00:08:17 +0200

    re

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar"
-import RecentActivity from "./features/dashboard/RecentActivity"
 
 function App() {
   return (
     <BrowserRouter>
     <Navbar />
       <Routes>
-      <Route path="/recent-activity" element={<RecentActivity />} />
       </Routes>
     </BrowserRouter>
   );
